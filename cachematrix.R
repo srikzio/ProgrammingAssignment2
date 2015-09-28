9a6958517fd967d84d18dc3e1ffdb37a40e726cf
srikzio <srikaanth23@gmail.com>
2015-09-28 10:06:52 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+##MakeCacheMatrix consists of four functions 1.create_matrix(cmat) 2.get_matrix(gmat) 3.set_inverse(sinv) 4.get_inverse(ginv)
+## cmat stores the input(values entered) in an variable
+## gmat converts the input (if its not by type matrix) and prints it in the matrix form ( row filling)
+## sinv stores the argument passed into this function as the inverse in a variable.( By default the value of inverse is null)
+## ginv converts the argument passed into sinv into matrix form if its possible ...otherwise it returns null by default
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+#cacheSolve function gets input from the object of makeCacheMatrix function and returns inverse if it already exists 
+##otherwise computes an inverse for the matrix and returns it
 
+
+## This function creates a matrix ( even if you don't input a matrix ) and sets in an inverse if given(stores whateve the user gives as inverse)
+
+makeCacheMatrix <- function(x = matrix()) {inv <- NULL
+                                           
+                                           create_matrix<-function(y){
+                                             x<<-y
+                                             inv<<-NULL
+                                             
+                                           }
+                                           get_matrix<-function(){
+                                             if(is.matrix(x)){x}
+                                             else {
+                                               dim(x)<-c(sqrt(length(x)),sqrt(length(x)))
+                                               x}
+                                           } 
+                                           set_inverse<-function(inverse) inv<<-inverse
+                                           get_inverse<-function() {
+                                             if(length(inv)==0)
+                                             {inv}
+                                             else if (is.matrix(inv)){inv}
+                                             else {
+                                               dim(inv)<-c(sqrt(length(inv)),sqrt(length(inv)))
+                                               inv
+                                             }
+                                             
+                                           }
+                                           
+                                           list(cmat=create_matrix,gmat=get_matrix,sinv=set_inverse,ginv=get_inverse)
+                                           
 }
 
 
-## Write a short comment describing this function
+##cacheSolve function gets its input from makeCacheMatrix function and checks if an inverse has been set for the matrix.
+##If it finds that an inverse has been set, it jus returns that value of the inverse without computing anything
+##If no inverse has been set, this function computes the inverse for the matrix using the solve function and returns it
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv<-x$ginv()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  matrix<-x$gmat()
+  inv<-solve(matrix) ##computes inverse
+  inv
 }
